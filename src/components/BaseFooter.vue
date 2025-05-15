da3891943e93205257ca5dcb4c0a6512a9d5a05e
Annie794922 <102657037+Annie794922@users.noreply.github.com>
2025-05-15 15:55:25 +0800

    [NEW] Add the component named BaseFooter

---
+++ src/components/BaseFooter.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="text-secondary small mb-3">
+        © 2025 Annie Chang.<br />
+        本網頁作品謹為個人學習用途，無商業行為。<br />
+        The side project is for learning purpose instead of commercial use.
+    </footer>
+</template>
