d6221e22ddec99343e73ae35eda2bc2429c099d0
Annie794922 <102657037+Annie794922@users.noreply.github.com>
2025-05-15 15:02:59 +0800

    [NEW] Add the component named HintMessage

---
+++ src/components/HintMessage.vue
@@ -0,0 +1,16 @@
+<template>
+    <p :class="[color, size]">
+        <i class="bi" :class="icon"></i>
+        {{ text }}
+    </p>
+</template>
+<script>
+export default {
+    props: {
+        icon: String,
+        size: String,
+        color: String,
+        text: String
+    }
+}
+</script>
