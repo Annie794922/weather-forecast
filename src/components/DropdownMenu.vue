1f8a8f871387f898745d222020f7abc892573c96
Annie794922 <102657037+Annie794922@users.noreply.github.com>
2025-05-19 13:47:13 +0800

    [FIX] Adjust the html structure of DropdownMenu for RWD in smaller view

--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -5,36 +5,41 @@
             {{ name }}
         </h5>
         <!-- The selected value and the options in menu -->
-        <div class="btn-group w-50">
-            <button
-                class="btn btn-success dropdown-toggle"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-            >
-                {{ selectedItem || '請選擇' }}
-            </button>
-            <ul v-if="name.includes('縣市')" class="dropdown-menu w-100">
-                <template v-for="(item, index) in items" :key="index">
-                    <li>
-                        <a class="dropdown-item" @click="getSelectedItem(item)">
-                            {{ item }}
-                        </a>
-                    </li>
-                </template>
-            </ul>
-            <ul v-if="name.includes('時段')" class="dropdown-menu w-100">
-                <template v-for="(item, index) in items" :key="index">
-                    <li>
-                        <a
-                            class="dropdown-item"
-                            @click="getSelectedItem(item.label)"
-                        >
-                            {{ item.label }}
-                        </a>
-                    </li>
-                </template>
-            </ul>
+        <div class="col-9 col-md-6 mx-auto">
+            <div class="btn-group w-100">
+                <button
+                    class="btn btn-success dropdown-toggle"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                >
+                    {{ selectedItem || '請選擇' }}
+                </button>
+                <ul v-if="name.includes('縣市')" class="dropdown-menu w-100">
+                    <template v-for="(item, index) in items" :key="index">
+                        <li>
+                            <a
+                                class="dropdown-item"
+                                @click="getSelectedItem(item)"
+                            >
+                                {{ item }}
+                            </a>
+                        </li>
+                    </template>
+                </ul>
+                <ul v-if="name.includes('時段')" class="dropdown-menu w-100">
+                    <template v-for="(item, index) in items" :key="index">
+                        <li>
+                            <a
+                                class="dropdown-item"
+                                @click="getSelectedItem(item.label)"
+                            >
+                                {{ item.label }}
+                            </a>
+                        </li>
+                    </template>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
