70b1c6b80b89a7039f9c49f6c84480e4916ba82a
Annie794922 <102657037+Annie794922@users.noreply.github.com>
2025-07-21 15:32:44 +0800

    [NEW] Add the color settings with SCSS

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,14 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+    background-color: map-get($backgroundColors, website);
 }
 
 nav {
