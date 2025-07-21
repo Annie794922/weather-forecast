6dd5a14646f2b1fdf0e02e6929d7484cbe0a87ab
Annie794922 <102657037+Annie794922@users.noreply.github.com>
2025-07-21 18:04:26 +0800

    [NEW] Add the router settings for AboutPage

--- src/components/BaseNavbar.vue
+++ src/components/BaseNavbar.vue
@@ -52,13 +52,13 @@ export default {
                     color: 'text-white',
                     path: '/',
                     activeClass: 'reading-page'
+                },
+                {
+                    title: 'About',
+                    color: 'text-white',
+                    path: '/about',
+                    activeClass: 'reading-page'
                 }
-                // {
-                //     title: 'About',
-                //     color: 'text-white',
-                //     path: '/about',
-                //     activeClass: 'reading-page'
-                // }
             ]
         }
     }
